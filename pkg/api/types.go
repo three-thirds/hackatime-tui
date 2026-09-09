46dda7924f2646214b22c3dc08b080fad3a098dc
William Gu <williamnumismatics@gmail.com>
2026-09-09 17:37:55 +1000

    finished up the last 7 days struct

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -19,21 +19,49 @@ type StatusToday struct {
 }
 
 type StatItem struct {
-	Key  string  `json:"key"`
+	Key   string  `json:"key"`
 	Total float64 `json:"total"`
 }
 
 type WakaResponse struct {
-		UserID string `json:"user_id"`
-		From time.Time `json:"from"`
-		To time.Time `json:"to"`
-		Projects []StatItem `json:"projects"`
-		Languages []StatItem `json:"languages"`
+	UserID    string     `json:"user_id"`
+	From      time.Time  `json:"from"`
+	To        time.Time  `json:"to"`
+	Projects  []StatItem `json:"projects"`
+	Languages []StatItem `json:"languages"`
 }
 
+type Days7Thing struct {
+	Name         string  `json:"name"`
+	TotalSeconds float64 `json:"total_seconds"`
+	Percent      float64 `json:"percent"`
+	DigitalTime  string  `json:"digital"`
+	TextTime     string  `json:"text"`
+	Hours        float64 `json:"hours"`
+	Minutes      float64 `json:"minutes"`
+	Seconds      float64 `json:"seconds"`
+}
 
 type Last7Days struct {
 	Data struct {
-		UserID string `json:"user_id"`
+		Username                  string       `json:"username"`
+		UserID                    string       `json:"user_id"`
+		Start                     time.Time    `json:"start"`
+		End                       time.Time    `json:"end"`
+		Status                    string       `json:"status"`
+		TotalSecs                 float64      `json:"total_seconds"`
+		DailyAverageSecs          float64      `json:"daily_average_seconds"`
+		DaysIncludingHolidays     int          `json:"days_including_holidays"`
+		Range                     string       `json:"range"`
+		HumanReadableRange        string       `json:"human_readable_range"`
+		HumanReadableTotal        string       `json:"human_readable_total"`
+		HumanReadableDailyAverage string       `json:"human_readable_daily_average"`
+		IsCodingActivityVisible   bool         `json:"is_coding_activity_visible"`
+		IsOtherUsageVisible       bool         `json:"is_other_usage_visible"`
+		Editors                   []Days7Thing `json:"editors"`
+		Languages                 []Days7Thing `json:"languages"`
+		Projects                  []Days7Thing `json:"projects"`
+		OperatingSystems          []Days7Thing `json:"operating_systems"`
+		Categories                []Days7Thing `json:"categories"`
 	} `json:"data"`
 }
